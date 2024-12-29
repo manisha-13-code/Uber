d48b9dc13da3351a688487207e4879c8314cd281
manisha-13-code <manishachauhan5469@gmail.com>
2024-12-29 23:36:20 +0530

    commit changes

---
+++ Backend/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT).then(()=> {
+        console.log('Connected to the database');
+    }).catch((err)=> {
+        console.log('An error occurred: ', err);
+    });
+}
+
+module.exports = connectToDb;
