c168192ebbefcbdea85ed479f96362669a385a5a
manisha-13-code <manishachauhan5469@gmail.com>
2025-01-02 23:01:55 +0530

    commit

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,12 +1,19 @@
 const userModel = require('../models/user.models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const blacklistToken = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next)=> {
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     if(!token) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
+
+    // const isBlacklisted = await blacklistToken.findOne({ token: token})
+
+    // if(isBlacklisted) {
+    //     return res.status(401).json({ message: 'Unauthorized' });
+    // }
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decoded._id);
