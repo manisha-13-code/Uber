68f23a7e1a317cedfb8abd50e1275d10fab81499
manisha-13-code <manishachauhan5469@gmail.com>
2025-02-16 20:26:12 +0530

    commit

---
+++ Frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const CaptainDetails = () => {
+  return (
+    <div>
+
+<div className='flex items-center justify-between'>
+          <div className='flex items-center justify-start gap-3'>
+            <img className='h-10 w-10 rounded-full object-cover' src="https://imgs.search.brave.com/h0NzAlc6Ad_QwzfEPRdT2kDTYBEsdN8oqeApk5-PfvA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzQzLzEyLzM0/LzM2MF9GXzI0MzEy/MzQ2M196VG9vdWI1/NTd4RVdBQkRMazBq/SmtsRHlMU0dsMmpy/ci5qcGc" alt="" />
+            <h4 className='text-lg font-medium'>Harsh Patel</h4>
+          </div>
+
+          <div>
+            <h4 className='text-xl font-semibold'>₹295.20</h4>
+            <p className='text-sm text-gray-600'>Earned</p>
+          </div>
+        </div>
+
+        <div className='flex p-3 bg-gray-100 rounded-xl justify-center gap-5 items-start mt-8'>
+          <div className='text-center'>
+            <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+            <h5 className='text-lg font-medium'>10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p>
+          </div>
+          <div className='text-center'>
+            <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+            <h5 className='text-lg font-medium'>10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p>
+            </div>
+          <div className='text-center'>
+            <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+            <h5 className='text-lg font-medium'>10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CaptainDetails
--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
 
 const CaptainHome = () => {
   return (
@@ -13,41 +15,15 @@ const CaptainHome = () => {
         </Link>
       </div>
 
-      <div className='h-1/2'>
+      <div className='h-3/5'>
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
+      <div className='h-2/5 p-6'>
+        <CaptainDetails />
+      </div>
 
-      <div className='h-1/2 p-5'>
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center justify-start gap-3'>
-            <img className='h-10 w-10 rounded-full object-cover' src="https://imgs.search.brave.com/h0NzAlc6Ad_QwzfEPRdT2kDTYBEsdN8oqeApk5-PfvA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzQzLzEyLzM0/LzM2MF9GXzI0MzEy/MzQ2M196VG9vdWI1/NTd4RVdBQkRMazBq/SmtsRHlMU0dsMmpy/ci5qcGc" alt="" />
-            <h4 className='text-lg font-medium'>Harsh Patel</h4>
-          </div>
-
-          <div>
-            <h4 className='text-xl font-semibold'>₹295.20</h4>
-            <p className='text-sm text-gray-600'>Earned</p>
-          </div>
-        </div>
-
-        <div className='flex p-3 bg-gray-100 rounded-full justify-center gap-5 items-start mt-5'>
-          <div className='text-center'>
-            <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hours Online</p>
-          </div>
-          <div className='text-center'>
-            <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hours Online</p>
-            </div>
-          <div className='text-center'>
-            <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hours Online</p>
-            </div>
-        </div>
-
+      <div className='fixed w-full z-10 bottom-0 bg-white px-3 py-10'>
+        <RidePopUp />
       </div>
     </div>
   )
