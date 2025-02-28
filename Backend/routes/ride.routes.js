613480370e32de4716ae02a01f981b1549979d50
manisha-13-code <manishachauhan5469@gmail.com>
2025-03-01 00:25:55 +0530

    commit

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const { body } = require('express-validator')
 
 router.post('/create', 
-    body('userId').isString
+    body('userId').isString().isLength({ min: 24, max: 24 }).withMessage('Invalid user id'),
+    body('pickup').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
+    body('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
 )
+
+module.exports = router;
